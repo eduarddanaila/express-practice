677eccbcd8843563f953e11e544cad85072b588c
eduarddanaila <eduardanaila04@gmail.com>
2022-10-07 12:23:24 +0100

    error handling

---
+++ error-handling/indexerror.js
@@ -0,0 +1,19 @@
+const express = require(`express`);
+const app = express();
+
+app.get(`/getError`, (req, res, next) => {
+    next(Error(`Error Massage`))
+});
+
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send(err.message);
+});
+
+app.listen(4000);
+
